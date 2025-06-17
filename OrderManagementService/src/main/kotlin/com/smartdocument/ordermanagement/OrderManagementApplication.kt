d95ebf4c9688084457fd8cdd897108ecce72a36f
Lakshmanan <it.lakshmanan@gmail.com>
2025-06-17 19:15:11 +0530

    Created Docker containers and micro-service packages

---
+++ OrderManagementService/src/main/kotlin/com/smartdocument/ordermanagement/OrderManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.smartdocument.ordermanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderManagementApplication>(*args)
+} 
