3a82fe3c4f2f26db06581172a2a9726d129be4d5
Lakshmanan <it.lakshmanan@gmail.com>
2025-06-20 02:03:00 +0530

    Fixed createBook issue

--- BookInventoryService/src/main/kotlin/com/smartdocument/bookinventory/mapper/BookMapper.kt
+++ BookInventoryService/src/main/kotlin/com/smartdocument/bookinventory/mapper/BookMapper.kt
@@ -8,8 +8,9 @@ import org.mapstruct.Mapping
 import org.mapstruct.Mappings
 import org.mapstruct.factory.Mappers
 import java.time.LocalDate
+import java.time.LocalDateTime
 
-@Mapper(componentModel = "spring", imports = [LocalDate::class])
+@Mapper(componentModel = "spring", imports = [LocalDate::class, LocalDateTime::class])
 interface BookMapper {
     companion object {
         val INSTANCE: BookMapper = Mappers.getMapper(BookMapper::class.java)
@@ -18,16 +19,16 @@ interface BookMapper {
     @Mappings(
         Mapping(target = "id", ignore = true),
         Mapping(target = "publishedDate", expression = "java(LocalDate.parse(dto.getPublishedDate()))"),
-        Mapping(target = "createdAt", ignore = true),
-        Mapping(target = "updatedAt", ignore = true)
+        Mapping(target = "createdAt", expression = "java(LocalDateTime.now())"),
+        Mapping(target = "updatedAt", expression = "java(LocalDateTime.now())")
     )
     fun toEntity(dto: BookRequestDto): Book
 
     @Mappings(
         Mapping(target = "id", source = "id"),
         Mapping(target = "publishedDate", expression = "java(LocalDate.parse(dto.getPublishedDate()))"),
-        Mapping(target = "createdAt", ignore = true),
-        Mapping(target = "updatedAt", ignore = true)
+        Mapping(target = "createdAt", expression = "java(LocalDateTime.now())"),
+        Mapping(target = "updatedAt", expression = "java(LocalDateTime.now())")
     )
     fun toEntityWithId(dto: BookRequestDto, id: Long): Book
 
